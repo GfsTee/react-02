61295ef094e796cf43caa31fa00a258772326a62
GFSTee <georg.treitz@gmail.com>
2020-11-24 11:57:38 +0100

    arrow with curly braces

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -5,17 +5,20 @@ const Section3 = () => {
     return (
         <div className="grid">
             {/* <Card3 /> */}
-            {data.map(user => <Card3
-                userFirst={user.first_name}
-                userLast={user.last_name}
-                userImg={user.image}
-                userColor={user.color}
-                userFullName={user.first_name + " " + user.last_name}
-                userFullName2={`${user.first_name} ${user.last_name}`}
-                key={user.id}
-            // userFullName3={userFirst + " " + userLast} funktioniert nicht
+            {data.map(user => {
 
-            />)}
+                return <Card3
+                    userFirst={user.first_name}
+                    userLast={user.last_name}
+                    userImg={user.image}
+                    userColor={user.color}
+                    userFullName={user.first_name + " " + user.last_name}
+                    userFullName2={`${user.first_name} ${user.last_name}`}
+                    key={user.id}
+                // userFullName3={userFirst + " " + userLast} funktioniert nicht
+
+                />
+            })}
         </div>
     );
 }
